7dfd018346b12f9401c5133c27251d05bdb82ef6
alvinsj <alvin.nsj@gmail.com>
2014-07-24 17:19:01 +0700

    better spec for deliver service object

--- spec/services/delivery_uncle/deliver_spec.rb
+++ spec/services/delivery_uncle/deliver_spec.rb
@@ -9,7 +9,7 @@ describe DeliveryUncle::Deliver do
     TestMailer.class_eval do
       default from: 'test@deliveryuncle.localhost'
   
-      def test_mail(subject)
+      def test_email_method(subject)
         mail(subject: subject, to: 'recepient@deliveryuncle.localhost')
       end
     end
@@ -20,7 +20,13 @@ describe DeliveryUncle::Deliver do
   end
 
   it "should receive email request" do
-    delivery = DeliveryUncle::Deliver.new(TestMailer, :test_mail, 'test_email')
+    delivery = DeliveryUncle::Deliver.new(TestMailer, :test_email_method, 'test_email')
+    
+    expect(delivery.request.mailer.to_s).to eql('TestMailer')
+    expect(delivery.request.mailer_method.to_s).to eql('test_email_method')
+    expect(delivery.request.mail.subject).to eql('test_email')
+    expect(delivery.request.status).to eql(:queued)
+    
     DeliveryUncle::SendEmailRequest.should have_queued(delivery.request.id)
   end
 end
